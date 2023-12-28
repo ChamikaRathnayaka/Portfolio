a51c9b4030c351e4c89047bdf01c1c6c9ba1d954
ChamikaRathnayaka <chamikarath97@gmail.com>
2023-12-28 21:20:58 +0530

    skills section done

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -22,12 +22,15 @@ export default function Skills() {
             </div>
             <div className="skill-details">
               <div class="row skills_grid">
-                <div class="col-sm-3 individual-skill glow">ss</div>
-                <div class="col-sm-3 individual-skill glow">ss</div>
+                <div class="col-sm-3 individual-skill glow">Spring Boot</div>
+                <div class="col-sm-3 individual-skill glow">Java Script</div>
               </div>
               <div class="row skills_grid">
-                <div class="col-sm-3 individual-skill glow ">ss</div>
-                <div class="col-sm-3 individual-skill glow">ss</div>
+                <div class="col-sm-3 individual-skill glow ">React</div>
+                <div class="col-sm-3 individual-skill glow">Node js</div>
+              </div>
+              <div class="row skills_grid">
+                <div class="col-sm-3 individual-skill glow ">Bootstrap</div>
               </div>
             </div>
           </div>
@@ -35,16 +38,25 @@ export default function Skills() {
         <div class="col-sm-6">
           <div class="card skills_card">
             <div className="card-header">
-              <h3>Web Development</h3>
+              <h3>Programming languages</h3>
             </div>
             <div className="skill-details">
               <div class="row skills_grid">
-                <div class="col-sm-3 individual-skill glow">ss</div>
-                <div class="col-sm-3 individual-skill glow">ss</div>
+                <div class="col-sm-3 individual-skill glow">Java</div>
+                <div class="col-sm-3 individual-skill glow">C</div>
               </div>
+            </div>
+          </div>
+          <div class="card skills_card">
+            <div className="card-header">
+              <h3>Database</h3>
+            </div>
+            <div className="skill-details">
               <div class="row skills_grid">
-                <div class="col-sm-3 individual-skill glow">ss</div>
-                <div class="col-sm-3 individual-skill glow">ss</div>
+                <div class="col-sm-3 individual-skill glow">MySQL</div>
+                <div class="col-sm-3 individual-skill glow">MSSQL</div>
+                <div class="col-sm-3 individual-skill glow">Oracle</div>
+                <div class="col-sm-3 individual-skill glow">MongoDB</div>
               </div>
             </div>
           </div>
